eb36efd689f597c95820bf30ae80e5bfb931faf6
Andrew Farmer <ahfarmer@gmail.com>
2018-07-27 15:02:55 -0700

    formatting

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
-const express = require('express');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const webpack = require('webpack');
-const webpackConfig = require('./webpack.config.js');
+const express = require("express");
+const webpackDevMiddleware = require("webpack-dev-middleware");
+const webpack = require("webpack");
+const webpackConfig = require("./webpack.config.js");
 const app = express();
 
 const compiler = webpack(webpackConfig);
 
-app.use(webpackDevMiddleware(compiler, {
-  hot: true,
-  filename: 'bundle.js',
-  publicPath: '/',
-  stats: {
-    colors: true,
-  },
-  historyApiFallback: true,
-}));
+app.use(
+  webpackDevMiddleware(compiler, {
+    hot: true,
+    filename: "bundle.js",
+    publicPath: "/",
+    stats: {
+      colors: true
+    },
+    historyApiFallback: true
+  })
+);
 
-app.use(express.static(__dirname + '/www'));
+app.use(express.static(__dirname + "/www"));
 
 const server = app.listen(3000, function() {
   const host = server.address().address;
   const port = server.address().port;
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log("Example app listening at http://%s:%s", host, port);
 });
